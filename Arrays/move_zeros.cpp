08c95eb31078b80ab37266b099a84f72780979bd
AyushAJS <jangidayush89@gmail.com>
2026-04-02 19:13:33 +0530

    Move Zeros Problem

---
+++ Arrays/move_zeros.cpp
@@ -0,0 +1,32 @@
+//   -----------------       Missing Element     --------------------------
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+
+void moveZeros(vector<int>& arr, int n) {
+    int j = 0;
+
+    for(int i = 0;  i < n; i++) {
+        if(arr[i] != 0) {
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+
+}
+
+int main() {
+    vector<int>arr = {};
+    int n = arr.size();
+
+    sort(arr.begin(), arr.end());
+
+    moveZeros(arr, n);
+
+
+    return 0;
+}
