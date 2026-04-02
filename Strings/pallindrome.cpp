b92d68be40b9476ac3c72f7557d2680d6c00c71a
AyushAJS <jangidayush89@gmail.com>
2026-04-02 21:34:01 +0530

    Check Pallindrome Problem

---
+++ Strings/pallindrome.cpp
@@ -0,0 +1,23 @@
+// -----------------------------------    Check Pallindrome in String    -------------------------------
+
+#include<iostream>
+
+using namespace std;
+
+bool checkPallindrom(string& str) {
+    int n = str.length();
+
+    for(int i = 0; i < n/2; i++) {
+        if(str[i] != str[n - i - 1]) return false;
+    }
+    
+    return true;
+}
+
+int main() {
+    string str = "CAR";
+
+    cout<<"Is "<<str<<" Pallindrome: "<<checkPallindrom(str);
+
+    return 0;
+}
